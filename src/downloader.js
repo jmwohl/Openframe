40c78f69bba6e6c1c9916535513802abf890afbd
Jonathan Wohl <jon@jonwohl.com>
2016-06-21 22:30:42 -0400

    Download with raw wget

--- src/downloader.js
+++ src/downloader.js
@@ -5,20 +5,9 @@
  */
 
 // Dependencies
-var exec = require('child_process').exec,
+var pexec = require('./process-manager').pexec,
     debug = require('debug')('openframe:downloader'),
-    artworkDir = '/tmp',
-    http = require('http-request');
-
-// unused at present
-function _mkdirp(dir) {
-    var mkdir = 'mkdir -p ' + dir;
-    exec(mkdir, function(err) {
-        if (err) {
-            throw err;
-        }
-    });
-}
+    artworkDir = '/tmp';
 
 /**
  * Download a file using HTTP get.
@@ -31,19 +20,9 @@ function downloadFile(file_url, file_output_name, cb) {
         var file_name = file_output_name,
             file_path = artworkDir + '/' + file_name;
 
-        // simplified download using http-request module
-        http.get({
-            url: file_url,
-            progress: function (current, total) {
-                debug('downloaded %d bytes from %d', current, total);
-            }
-        }, file_path, function (err, res) {
-            if (err) {
-                reject(err);
-                return;
-            }
-            resolve(res.file);
-        });
+        pexec('wget -O ' + file_path + ' ' + file_url)
+            .then(resolve)
+            .catch(reject);
     });
 
 }
